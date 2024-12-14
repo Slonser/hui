5784f3657379ddbe1c8f42d6b6e4fd8a0fd3b05d
slonser <sevakokorin80@gmail.com>
2024-12-14 18:39:37 +0300

    Add 0.2.1 build

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'hui',
   packages= find_packages(),
-  version = '0.2.0',
+  version = '0.2.1',
   license='MIT', 
   author = 'Slonser',
   author_email = 'slonser@slonser.info',
